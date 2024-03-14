cf858d4b5b10ff8daf548b45e468f16b3a4614ea
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2024-03-14 21:16:47 +0200

    complete task

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,53 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+}
+
+class Person {
+  private Key: Key;
+  constructor(key: Key) {
+    this.Key = key;
+  }
+  getKey() {
+    return this.Key;
+  }
+}
+
+abstract class House {
+  protected Door: boolean;
+  protected Key: Key;
+  protected Tenants: Person[];
+
+  constructor(key: Key) {
+    this.Door = false;
+    this.Key = key;
+    this.Tenants = [];
+  }
+    
+    abstract OpenDoor(key: Key): void;
+
+  comeIn(person: Person) {
+      if (this.Door) {
+        this.Tenants.push(person)
+    }
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  OpenDoor(key: Key) {
+    if (key === this.Key) {
+      this.Door = true;
+    }
+
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +56,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
